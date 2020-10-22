ecf28ab903d2ee94bf5404de41700a302bf48514
Eduardo Lima <eduardocoelholima@gmail.com>
2020-10-22 19:44:54 -0400

    4th commit

--- src/Password.java
+++ src/Password.java
@@ -2,37 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Password implements Serializable {
-  private int MINIMUMlength = 10;
-  private String password;
-  private int    hashCode;
+    private int MINIMUMlength = 10;
+    private String password;
+    private int    hashCode;
 
-  public Password(String password)	{
-		this.password = password;
-	        this.hashCode = password.hashCode();
-  }
-  private void writeObject(ObjectOutputStream s) throws IOException {
-	System.out.println("Password: writeObject");
-	s.defaultWriteObject();
+    public Password(String password)	{
+        this.password = password;
+        this.hashCode = password.hashCode();
+    }
+
+    private void writeObject(ObjectOutputStream s) throws IOException {
+        System.out.println("Password: writeObject");
+        s.defaultWriteObject();
         s.writeObject(password);
         s.writeObject(hashCode);
-  }
+    }
 
-  private void readObject(ObjectInputStream s) throws IOException  {
-	System.out.println("Password: readObject");
-	try {
-         	s.defaultReadObject();
-		String password = (String)s.readObject();
-		int    hashCode = (int)s.readObject();
-		if ( this.password.hashCode() != hashCode )
-			this.password = "";
-	}
-        catch ( ClassNotFoundException e)	{
-    	    System.out.println(e.getMessage());
-    	    e.printStackTrace();
+    private void readObject(ObjectInputStream s) throws IOException  {
+        System.out.println("Password: readObject");
+        try {
+            s.defaultReadObject();
+            String password = (String)s.readObject();
+            int    hashCode = (int)s.readObject();
+            if ( this.password.hashCode() != hashCode )
+                this.password = "";
         }
-  }
-  public String toString()	{
-	return password;
-  }
+        catch ( ClassNotFoundException e)	{
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    public String toString()	{
+        return password;
+    }
 
 }
--- src/PasswordRead.java
+++ src/PasswordRead.java
@@ -3,26 +3,26 @@ import java.util.*;
 
 public class PasswordRead {
 
-  public static void main( String args[] ) {
+    public static void main( String args[] ) {
 
-    Password aPassword;
-    String fileName = "1234.ser";
+        Password aPassword;
+        String fileName = "1234.ser";
 
-    if ( args.length == 1 )
-	fileName = args[0];
-    try (
-        ObjectInputStream p = new ObjectInputStream(new FileInputStream(fileName) );
-	) {
-        aPassword = (Password)p.readObject();
-	System.out.println("aPassword = " + aPassword );
+        if ( args.length == 1 )
+            fileName = args[0];
+        try (
+                ObjectInputStream p = new ObjectInputStream(new FileInputStream(fileName) );
+        ) {
+            aPassword = (Password)p.readObject();
+            System.out.println("aPassword = " + aPassword );
+        }
+        catch ( IOException e)	{
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        catch ( ClassNotFoundException e)	{
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
-    catch ( IOException e)	{
-    	System.out.println(e.getMessage());
-	e.printStackTrace();
-    }
-    catch ( ClassNotFoundException e)	{
-    	System.out.println(e.getMessage());
-	e.printStackTrace();
-    }
-  }
 }
